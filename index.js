9d4d5cd736c1dfe83066ce52550bee21c670c5ca
Tiana Aldroubi <tianama02@gmail.com>
2024-03-06 20:08:37 -0500

    p2

---
+++ index.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var links = document.querySelectorAll('.nav-link');
+    
+    links.forEach(function (link) {
+        link.addEventListener('click', function () {
+            // Remove 'active' class from all links
+            links.forEach(function (otherLink) {
+                otherLink.classList.remove('active');
+            });
+
+            // Add 'active' class to the clicked link
+            link.classList.add('active');
+        });
+    });
+});
